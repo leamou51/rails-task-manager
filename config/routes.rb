724774a774dee75eefd761c3953834f248cb97af
lea mousel <leamousel51@gmail.com>
2020-08-11 12:32:27 +0200

    task manager web app with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# CREATE
+get "tasks/new", to: "tasks#new"
+post "tasks", to: "tasks#create"
+
+# READ
+get "tasks", to: "tasks#index"
+get "tasks/:id", to: "tasks#show", as: :task
+
+# UPDATE
+get "tasks/:id/edit", to: "tasks#edit", as: :edit_restaurant
+patch "tasks/:id", to: "tasks#update"
+
+# DELETE
+delete "tasks", to: "tasks#destroy"
+
 end
